52bef69ab9f1e845d0b5743ce65fe9be789c2554
chethanm933/chethanm933 <chethanm933@gmail.com>
2021-12-12 09:39:40 +0530

    modified Employeewage UC1

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -3,7 +3,11 @@ package EmployeeWage;
 public class EmployeeWageComputation {
 	
 	public static void main(String[] args) {
+		
+		int present = 1;
+		int paarttime = 2;
+		
 		EmployeeUC1 emp = new EmployeeUC1();
-		emp.PresentAbsent(1, 2);	}
+		emp.PresentAbsent(present, paarttime);	}
 
 }
